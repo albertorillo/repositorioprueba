9db63f89b25d999837baa002e789c9172efd245f
arillo <arillo@ES78CG6050FG6.es.kworld.kpmg.com>
2016-12-23 14:18:18 +0100

    Commit2

--- prueba.php
+++ prueba.php
@@ -6,10 +6,10 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <title>2</title>
+        <title>23/12/2016</title>
     </head>
     <body>
-        2
+        23/12/2016
     </body>
     <script></script>
 </html>
